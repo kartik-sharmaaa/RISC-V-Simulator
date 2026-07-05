6b672046842656c74481e23123990040599b2ff1
kartik-sharmaaa <115631703+kartik-sharmaaa@users.noreply.github.com>
2026-07-05 12:09:56 +0530

    commit

--- pipeline_webapp/server/knobs.py
+++
@@ -1,3 +0,0 @@
-data_forwarding: int = 0
-print_rf: int = 0
-print_pipeline: int = 0
--- pipeline_webapp/server/pipeline_registers/IF_DE.py
+++
@@ -1,9 +0,0 @@
-instruction: str = None
-pc: int = None
-
-nop: int = 0
-
-
-def init() -> None:
-    global instruction, pc, nop
-    (instruction, pc, nop) = (None, None, None)
